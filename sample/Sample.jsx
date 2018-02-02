04001fb74b0c3b303a21e9a116fb4f6bbd18a71d
Ryan Sydnor <ryan.t.sydnor@gmail.com>
2018-02-01 23:38:16 -0500

    Multi pdf example

--- sample/Sample.jsx
+++ sample/Sample.jsx
@@ -5,66 +5,62 @@ import 'react-pdf/dist/Page/AnnotationLayer.css';
 
 import './Sample.less';
 
+const urls = [
+  'https://preview.teacherspayteachers.com/0/968/968647/1472240265_demowinterkindergartenpreviewmoffattgirlspdf.pdf?h-cbpb-2wtmgJxnJrGIwQvHQreTcHQeF48TU54Ibl-wHw22I_rL7WYIIxwz-2ll7&file_name=demowinterkindergartenpreviewmoffattgirlspdf.pdf',
+  'https://preview.teacherspayteachers.com/1/1563/1563281/1472231909_demo2ndgradedecembertptpreviewpdf.pdf?qsI6uuLD2ur6MpnlnGZSj4WI1m7bRW1O97xbSPD_KwoN5h8CVCuO-1HYUimUTRea&file_name=demo2ndgradedecembertptpreviewpdf.pdf',
+  'https://preview.teacherspayteachers.com/0/998/998189/1472234935_demopreview1stgradenoprepdecemberpdf.pdf?1ABSyCqpZHZIOvmG87qNuDm5-P1bS2Il2SNHHE3MY0vq2IGPZ30MaVCFIfts-zaY&file_name=demopreview1stgradenoprepdecemberpdf.pdf'
+];
+
 setOptions({
   cMapUrl: 'cmaps/',
   cMapPacked: true,
 });
 
 class Sample extends Component {
-  state = {
-    file: './sample.pdf',
-    numPages: null,
-  }
+  state = { numPages: null }
 
-  onFileChange = (event) => {
-    this.setState({
-      file: event.target.files[0],
-    });
-  }
-
-  onDocumentLoadSuccess = ({ numPages }) =>
-    this.setState({
-      numPages,
-    })
+  onDocumentLoadSuccess = ({ numPages }) => this.setState({ numPages })
 
   render() {
-    const { file, numPages } = this.state;
+    const { url } = this.props;
+    const { numPages } = this.state;
 
     return (
-      <div className="Example">
-        <header>
-          <h1>react-pdf sample page</h1>
-        </header>
-        <div className="Example__container">
-          <div className="Example__container__load">
-            <label htmlFor="file">Load from file:</label>&nbsp;
-            <input
-              type="file"
-              onChange={this.onFileChange}
-            />
-          </div>
-          <div className="Example__container__document">
-            <Document
-              file={file}
-              onLoadSuccess={this.onDocumentLoadSuccess}
-            >
-              {
-                Array.from(
-                  new Array(numPages),
-                  (el, index) => (
-                    <Page
-                      key={`page_${index + 1}`}
-                      pageNumber={index + 1}
-                    />
-                  ),
+      <div className="Example__container">
+        <div className="Example__container__document">
+          <Document
+            file={url}
+            onLoadSuccess={this.onDocumentLoadSuccess}
+          >
+            {
+              Array.from(new Array(numPages),
+                (el, index) => (
+                  <Page
+                    key={`page_${index + 1}`}
+                    pageNumber={index + 1}
+                  />
                 )
-              }
-            </Document>
-          </div>
+              )
+            }
+          </Document>
         </div>
       </div>
     );
   }
 }
 
-render(<Sample />, document.getElementById('react-container'));
+class Omg extends Component {
+
+  render() {
+    return(
+      <div className="Example">
+        <header>
+          <h1>react-pdf sample page</h1>
+        </header>  
+        { urls.map((url) => <Sample url={ url } />) }
+      </div>
+    );
+  }
+}
+
+render(<Omg />, document.getElementById('react-container'));
--- sample/webpack.config.js
+++ sample/webpack.config.js
@@ -38,17 +38,25 @@ module.exports = {
       },
     ],
   },
+  devServer: {
+    watchOptions: {
+      ignored: [
+        path.resolve(__dirname, 'dist'),
+        path.resolve(__dirname, 'node_modules')
+      ]
+    }
+  },
   plugins: [
     new webpack.DefinePlugin({
       'process.env': {
-        NODE_ENV: JSON.stringify('production'),
+        NODE_ENV: JSON.stringify('development'),
       },
     }),
     new webpack.optimize.UglifyJsPlugin(),
     new CopyWebpackPlugin([
       { from: './index.html' },
       { from: './sample.pdf' },
-      { from: 'node_modules/pdfjs-dist/cmaps/', to: 'cmaps/' },
+      { from: '../node_modules/pdfjs-dist/cmaps/', to: 'cmaps/' },
     ]),
   ],
 };
